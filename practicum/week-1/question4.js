af871549b3d3b5d3369ce856f02417a23f56458c
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-08-26 20:36:06 -0700

    q4

--- practicum/week-1/question4.js
+++ practicum/week-1/question4.js
@@ -11,5 +11,11 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
 */
 
 var howManyCommon = function(A, B) {
-
+    var total = 0;
+    for(i = 0; i < B.length; i++){
+        if (A.includes(B[i])){
+            total++;
+        }
+    }
+    return total;
 };
