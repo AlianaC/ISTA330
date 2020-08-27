a137f970d3a0937241573cf13c81c55854881b21
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-08-26 21:26:44 -0700

    q6 done

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -9,7 +9,6 @@ var median = function(input) {
     input.sort();
     if (input.length % 2 == 0){
         var x = input.length / 2;
-        alert("x: " + x + "\nFloor: " +  Math.floor(x) + "\nCeil: " + Math.ceil(x));
-        return (input[Math.floor(x)] + input[Math.ceil(x)]) / 2;
-    }return input[input.length / 2];
+        return (input[x] + input[x + 1]) / 2;
+    }return input[Math.floor(input.length / 2)];
 };
